4ea4a5a0b5e3f121084e52259dfa16c8bcf67bc4
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-07-11 01:22:09 +0000

    Update Compare-UserJS.bat

--- Compare-UserJS.bat
+++ Compare-UserJS.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 :: Place this in the same folder as Compare-UserJS.ps1 and run it. Provided only for the layman's convenience.
 TITLE %~n0
-IF NOT EXIST "%~n0.ps1" (
-	ECHO: Script not found in the current directory. Aborting.
-	GOTO :EOF)
-PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~pn0.ps1' %*"
+IF EXIST "%~n0.ps1" (
+	PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~pn0.ps1' %*"
+) ELSE ( ECHO: Script not found in the current directory. Aborting. )
+PAUSE
 
 :: PROtip: rename this script to match the name of any other PS script to run it. How cool is that?
