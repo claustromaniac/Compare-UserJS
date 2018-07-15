6012669e7ef2ebabefd79361f0555a8773147b42
claustromaniac <20734810+claustromaniac@users.noreply.github.com>
2018-07-15 07:17:04 +0000

    Update Compare-UserJS.bat

--- Compare-UserJS.bat
+++ Compare-UserJS.bat
@@ -1,9 +1,14 @@
 @ECHO OFF
 :: Place this in the same folder as Compare-UserJS.ps1 and run it. Provided only for the layman's convenience.
+
+:: This batch file launches the Compare-UserJS PowerShell script bypassing the PS execution policy.
+:: Any and all of the parameters that you pass to this script will be relayed to the PS script.
+:: You can even drag and drop the input files to this batch file (or to a shortcut pointing to it), if you want.
+
 TITLE %~n0
 IF EXIST "%~n0.ps1" (
 	PowerShell.exe -ExecutionPolicy Bypass -Command "& '%~pn0.ps1' %*"
 ) ELSE ( ECHO: Script not found in the current directory. Aborting. )
 PAUSE
 
-:: PROtip: rename this script to match the name of any other PS script to run it. How cool is that?
+:: PROTIP: rename this script to match the name of any other PS script to run it. How cool is that?
